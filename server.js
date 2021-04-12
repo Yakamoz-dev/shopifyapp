6e95c3b71e6f6d27edf4c694c4fc9f166100e762
close-7 <531210222@qq.com>
2021-04-12 17:58:42 +0800

    try oauth

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const dotenv = require("dotenv");
 const Koa = require("koa");
 const next = require("next");
 const { default: createShopifyAuth } = require("@shopify/koa-shopify-auth");
-const { verifyRequest } = require("@shopify/koa-shopify-auth");
+// const { verifyRequest } = require("@shopify/koa-shopify-auth");
+import shopifyAuth, {verifyRequest} from '@shopify/koa-shopify-auth'
 const { default: Shopify, ApiVersion } = require("@shopify/shopify-api");
 const Router = require("koa-router");
 const getSubscriptionUrl = require("./server/getSubscriptionUrl");
+
+import session from 'koa-session';
 
 dotenv.config();
 
@@ -27,45 +30,83 @@ const handle = app.getRequestHandler();
 
 const ACTIVE_SHOPIFY_SHOPS = {};
 
+
+
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
   server.keys = [Shopify.Context.API_SECRET_KEY];
 
+  const {SHOPIFY_API_KEY, SHOPIFY_SECRET} = process.env;
+
+  server.use(session({ secure: true, sameSite: 'none' }, server))
   server.use(
-    createShopifyAuth({
-      async afterAuth(ctx) {
-        const { shop, scope, accessToken } = ctx.state.shopify;
-        ACTIVE_SHOPIFY_SHOPS[shop] = scope;
-
-        const registration = await Shopify.Webhooks.Registry.register({
-          shop,
-          accessToken,
-          path: "/webhooks",
-          topic: "APP_UNINSTALLED",
-          apiVersion: ApiVersion.October20,
-          webhookHandler: (_topic, shop, _body) => {
-            console.log("App uninstalled");
-            delete ACTIVE_SHOPIFY_SHOPS[shop];
-          },
-        });
-
-        if (registration.success) {
-          console.log("Successfully registered webhook!");
-        } else {
-          console.log("Failed to register webhook", registration.result);
-        }
-
-        const returnUrl = `https://${Shopify.Context.HOST_NAME}?shop=${shop}`;
-        const subscriptionUrl = await getSubscriptionUrl(
-          accessToken,
-          shop,
-          returnUrl
-        );
-        ctx.redirect(subscriptionUrl);
+    shopifyAuth({
+      // if specified, mounts the routes off of the given path
+      // eg. /shopify/auth, /shopify/auth/callback
+      // defaults to ''
+      prefix: '/shopify',
+      // your shopify app api key
+      apiKey: SHOPIFY_API_KEY,
+      // your shopify app secret
+      secret: SHOPIFY_SECRET,
+      // scopes to request on the merchants store
+      scopes: ['write_orders, write_products'],
+      // set access mode, default is 'online'
+      // accessMode: 'offline',
+      // callback for when auth is completed
+      afterAuth(ctx) {
+        const {shop, accessToken} = ctx.session;
+   
+        console.log('We did it!', accessToken);
+   
+        ctx.redirect('/');
       },
-    })
+    }),
+    // createShopifyAuth({
+    //   async afterAuth(ctx) {
+    //     const { shop, scope, accessToken } = ctx.state.shopify;
+    //     ACTIVE_SHOPIFY_SHOPS[shop] = scope;
+
+    //     const registration = await Shopify.Webhooks.Registry.register({
+    //       shop,
+    //       accessToken,
+    //       path: "/webhooks",
+    //       topic: "APP_UNINSTALLED",
+    //       apiVersion: ApiVersion.October20,
+    //       webhookHandler: (_topic, shop, _body) => {
+    //         console.log("App uninstalled");
+    //         delete ACTIVE_SHOPIFY_SHOPS[shop];
+    //       },
+    //     });
+
+    //     if (registration.success) {
+    //       console.log("Successfully registered webhook!");
+    //     } else {
+    //       console.log("Failed to register webhook", registration.result);
+    //     }
+
+    //     const returnUrl = `https://${Shopify.Context.HOST_NAME}?shop=${shop}`;
+    //     const subscriptionUrl = await getSubscriptionUrl(
+    //       accessToken,
+    //       shop,
+    //       returnUrl
+    //     );
+    //     ctx.redirect(subscriptionUrl);
+    //   },
+    // })
   );
+  server.use(
+    verifyRequest({
+      // path to redirect to if verification fails
+      // defaults to '/auth'
+      authRoute: '/auth',
+      // path to redirect to if verification fails and there is no shop on the query
+      // defaults to '/auth'
+      fallbackRoute: '/index',
+    }),
+  );
+
 
   router.post("/graphql", verifyRequest(), async (ctx, next) => {
     await Shopify.Utils.graphqlProxy(ctx.req, ctx.res);

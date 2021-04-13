87c0fac416a41c4ce1f4df1953e0dae213281b9e
close-7 <531210222@qq.com>
2021-04-13 10:12:35 +0800

    redirect

--- pages/test.js
+++ pages/test.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Link from 'next/link'
+import { Link} from '@shopify/polaris';
+import {Redirect} from '@shopify/app-bridge/actions';
 
-
+const url = process.env.SHOPIFY_APP_URL
 
  const Test =() => {
-    
+    console.log(url)
     return (
-        <Link href="/">
-        <a>Index</a>
-      </Link>
+  <Link external onClick={()=>{redirect.dispatch(Redirect.Action.APP, '/settings');}} >
+    Index
+  </Link>
     );
 }
 
